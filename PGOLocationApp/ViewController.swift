b4729370e899cbee68d9f8481c191aba16a4e3a2
Simon Lipke <simon.lipke@googlemail.com>
2016-08-02 15:44:41 +0200

    Initial Commit

---
+++ PGOLocationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PGOLocationApp
+//
+//  Created by Simon Lipke on 02.08.16.
+//  Copyright © 2016 Simon Lipke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
